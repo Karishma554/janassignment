64694813625bbe7f3c90e9a04cab198994829145
Karishma554 <60257814+Karishma554@users.noreply.github.com>
2022-01-30 18:04:55 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+var a = 130;
+var b = 195;
+var c = 170;
+if(a>b && a>c){
+  console.log(a+" is maximum number ");
+}
+else if(b>c){
+   console.log(b+" is maximum number ");
+}
+else {
+   console.log(c+" is maximum number ");
+}
